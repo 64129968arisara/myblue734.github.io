f8c2878bcd233b88b6eccfb79b378ef806f8c083
myblue734 <myblue734@gmail.com>
2026-02-08 22:53:47 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/surprise-love/", // 👈 สำคัญมาก
+});
